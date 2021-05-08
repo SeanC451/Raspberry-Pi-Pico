f4ee0745c26971cc1958cc640e2ac076f4a0f3b9
Sean <77973833+SeanC451@users.noreply.github.com>
2021-05-08 10:43:52 -0400

    Update led-dice-single-tiny2040-stable.py
    Added function to flash on unlit LEDs and then turn them off in chase sequence.

--- led-dice-single-tiny2040-stable.py
+++ led-dice-single-tiny2040-stable.py
@@ -13,7 +13,7 @@ import machine, utime, random
 # Declarations
 
 timer = .05                              # Delay timer to flash LEDs
-hold = 1                                 # Delay timer after roll. Change to adjust.
+hold = 2                                 # Delay timer after roll. Change to adjust.
 
 LED_pips = [1,2,3,4,5,6]
 
@@ -23,6 +23,18 @@ for pin in LED_pips:
 button = machine.Pin(26, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 # Functions
+
+def zipPips():                             # Function to turn on remaning LEDs and then all off.
+    for x in range(0,6):
+        LED = machine.Pin(LED_pips[x])
+        if LED.value() != 1:
+            LED.toggle()
+            utime.sleep(timer)
+    utime.sleep(timer)
+    for x in range(5,-1,-1):
+        LED = machine.Pin(LED_pips[x])
+        LED.toggle()
+        utime.sleep(timer)
 
 def flashPips():                           # Function to turn LEDs on and off.
     for x in range(0,6):
@@ -35,33 +47,33 @@ def flashPips():                           # Function to turn LEDs on and off.
         utime.sleep(timer)
 
 def flashRoll():                           # Function to display random die roll.
-    print("Roll =",ran)                    # Debug/Comment out to stop Serial Print.
+    if (ran >= 1):
+        LED = machine.Pin(LED_pips[0])
+        LED.toggle()
+    if (ran >= 2):
+        LED = machine.Pin(LED_pips[1])
+        LED.toggle()
+    if (ran >= 3):
+        LED = machine.Pin(LED_pips[2])
+        LED.toggle()
+    if (ran >= 4):
+        LED = machine.Pin(LED_pips[3])
+        LED.toggle()
+    if (ran >= 5):
+        LED = machine.Pin(LED_pips[4])
+        LED.toggle()
+    if (ran >= 6):
+        LED = machine.Pin(LED_pips[5])
+        LED.toggle()
+    utime.sleep(hold)
+    zipPips()
 
-    for x in range(2):                     # Loop runs twice to toggle off LEDs.
-        if (ran >= 1):
-            LED = machine.Pin(LED_pips[0])
-            LED.toggle()
-        if (ran >= 2):
-            LED = machine.Pin(LED_pips[1])
-            LED.toggle()
-        if (ran >= 3):
-            LED = machine.Pin(LED_pips[2])
-            LED.toggle()
-        if (ran >= 4):
-            LED = machine.Pin(LED_pips[3])
-            LED.toggle()
-        if (ran >= 5):
-            LED = machine.Pin(LED_pips[4])
-            LED.toggle()
-        if (ran >= 6):
-            LED = machine.Pin(LED_pips[5])
-            LED.toggle()
-        utime.sleep(hold)
     
 # Loop
 
 while True:
     ran = random.randint(1, 6)
     if button.value() == 1:
+        print("Roll =",ran)                    # Debug/Comment out to stop Serial Print.
         flashPips()
         flashRoll()
